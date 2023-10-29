3844cc4dcdd946aaebf53043ab58a1391a64a02c
Eduardo Patricio Avila <pato_ab1990@hotmail.com>
2023-10-28 22:10:36 -0500

    feat(pre-commit): Added husku pre-commit hook and lint-staged

--- domains/home/src/lib/home.tsx
+++ domains/home/src/lib/home.tsx
@@ -3,9 +3,9 @@ import { Carousel, MostViewed } from '@swapi/components'
 export function Home() {
   return (
     <>
-    <div></div>
       <Carousel />
       <MostViewed />
+      <div></div>
     </>
   )
 }
